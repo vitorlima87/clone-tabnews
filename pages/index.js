52ab5b900849d16709e7e390730700ad19651375
vitorlima87 <vitor1652lima@gmail.com>
2025-12-16 03:01:07 +0000

    adicionar scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-    return (
-        <h1>
-            Que seu dia seja otimo  
-        </h1>
-    )
+  return <h1>Que seu dia seja otimo</h1>;
+}
+
+function teste2() {
+  console.log("teste parte 2 ");
 }
 
 export default Home;
