15d8022eaf689cecb2fce59dcb98ac6d24a17d89
vitorlima87 <vitor1652lima@gmail.com>
2026-01-21 14:25:30 +0000

    add database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).send("pessoas do curso,dev são pessoas acima da media");
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "são acima da media " });
 }
 
 export default status;
